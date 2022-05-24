94d0ba50726affc3bdd0ace3c11db58992b2ca69
kkatrina93 <kkatrina93@mail.ru>
2022-05-24 20:38:20 +0300

    Created a new metod

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 public class Program {
     public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList<>();
+        arrayList.add("Яблоко Слово");
+        arrayList.add("Апельсин");
+        arrayList.add("Слово");
+        arrayList.add("Ананас Слово");
+        arrayList.add("Виноград");
+        System.out.println(arrayList);
+        System.out.println(getElementsByWord(arrayList, "Слово"));
+
 
     }
+
+    public static ArrayList<String> getElementsByWord(ArrayList<String> arr, String world) {
+        return(ArrayList<String>)arr.stream().filter(a -> a.contains(world)).collect(Collectors.toList());
+          }
 }
